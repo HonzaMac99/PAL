0f4c3057df223902d6e2fd7d1385454ef8dd4045
macaljan_LinPC <macaljan@fel.cvut.cz>
2022-10-08 21:29:48 +0200

    add Cmake and test files

---
+++ task01/task01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "task01.hpp"
+
+int main(int argc, char* argv[])
+{
+  say_hello(argv[0]);
+  say_hello("Honza");
+  return 0;
+}
---
+++ task01/task01.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void say_hello(std::string name);
+
+void say_hello(std::string name) {
+  std::cout << "Hello, " << name << std::endl; 
+}
