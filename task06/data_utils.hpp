5d227fb5b40dff3d2578437acf055d8c8c3b3206
macaljan_LinNtb <macaljan@fel.cvut.cz>
2023-01-07 15:08:40 +0100

    added functions for reading from stdin

--- task06/data_utils.hpp
+++ task06/data_utils.hpp
@@ -4,7 +4,10 @@
 #include <iostream>
 #include "print_utils.hpp"
 
-#define DATA_INFO 0 
+#define DATA_INFO 1 
+
+typedef std::vector<int> VEC;
+typedef std::vector<vector<int>> VEC_2D;
 
 VEC_2D get_prod_matrix(int n_rows, int n_cols);
 VEC_2D get_clip_schemes(int n_clips);
@@ -12,15 +15,65 @@ VEC_2D get_clip_schemes(int n_clips);
 
 VEC_2D get_prod_matrix(int n_rows, int n_cols) {
   VEC_2D prod_matrix;
-
+  char new_link;
+  int new_link_len;
+  for(int i = 0; i < n_rows; i++) {
+    VEC prod_row; 
+    for(int j = 0; j < n_cols; j++) {
+      std::cin >> new_link;
+      new_link_len = new_link - 'a' + 1;
+      prod_row.push_back(new_link_len);
+    }
+    prod_matrix.push_back(prod_row);
+  }
+#if DATA_INFO
+  for(int i = 0; i < n_rows; i++) {
+    for(int j = 0; j < n_cols; j++) {
+      std::cout << prod_matrix[i][j] << " ";
+    } 
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+#endif
   return prod_matrix; 
 }
 
+VEC get_desr_chain(int desr_chain_len) {
+  VEC desr_chain;
+  char new_link;
+  int new_link_len;
+  for(int i = 0; i < desr_chain_len; i++) {
+    std::cin >> new_link;
+    new_link_len = new_link - 'a' + 1;
+    desr_chain.push_back(new_link_len);
+  }
+#if DATA_INFO
+  for(int i = 0; i < desr_chain_len; i++) {
+    std::cout << desr_chain[i] << " ";
+  }
+  std::cout << std::endl << std::endl;
+#endif
+  return desr_chain;
+}
 
 VEC_2D get_clip_schemes(int n_clips) {
- VEC_2D clip_schemes;
-
- return clip_schemes;
+  VEC_2D clip_schemes;
+  char new_link;
+  int new_link_len;
+  std::string clip_scheme_str;
+  for(int i = 0; i < n_clips; i++) {
+    VEC clip_scheme; 
+    std::cin >> clip_scheme_str; 
+    for(int j = 0; j < clip_scheme_str.size(); j++) {
+      new_link = clip_scheme_str[j];
+      new_link_len = new_link - 'a' + 1; 
+      clip_scheme.push_back(new_link_len);
+      std::cout << new_link_len << " ";
+    }
+    clip_schemes.push_back(clip_scheme);
+    std::cout << std::endl;
+  }
+  return clip_schemes;
 }
 #endif
 
--- task06/task06.cpp
+++ task06/task06.cpp
@@ -12,27 +12,17 @@ typedef std::vector<vector<int>> VEC_2D;
 int main()
 {
   // Do './task06 < ../datapub/pub01.in' for reading files using stdin
-  // No need for implementing reading from file in the program 
+  // No need for implementing reading from file in this program 
   
-  int n_rows, n_cols, f_chain_len, n_clips, clip_factor, rplc_factor;
-  std::cin >> n_rows >> n_cols >> f_chain_len >> n_clips >> clip_factor >> rplc_factor;
+  int n_rows, n_cols, desr_chain_len, n_clips, clip_factor, replace_factor;
 
-  //nfa_state* lex_nfa = get_stdin_data(n_states);
+  std::cin >> n_rows >> n_cols >> desr_chain_len >> n_clips 
+           >> clip_factor >> replace_factor;
   VEC_2D prod_matrix = get_prod_matrix(n_rows, n_cols);
-
-  std::string f_chain;
-  std::cin >> f_chain; 
-
+  VEC desr_chain = get_desr_chain(desr_chain_len);
   VEC_2D clip_schemes = get_clip_schemes(n_clips);
 
-  std::string substring; 
-  std::getline(std::cin, substring);
-  nfa_state* substr_nfa = get_substr_nfa(substring, alphabet_len);
-
-  int substr_len = substring.length();
-  std::string nfa_word = get_nfa_word(lex_nfa, substr_nfa, alphabet_len, substr_len);
-
-  std::cout << nfa_word << std::endl;
+  //std::cout << "0 0 0" << std::endl;
 
   return 0;
 }
